e673e801e726b1287dac03f070761eedd9abf9f6
jay winebrenner <jaywinebrenner@jays-iMac-2.local>
2020-10-12 09:27:33 -0700

    create initial layout

---
+++ src/components/Navbar.vue
@@ -0,0 +1,80 @@
+<template>
+
+<div class="wholeNavbarWrapper">
+  <div class="topNavbarWrapper">
+
+      <div class="navbarLeft">
+          Left
+      </div>
+
+      <div class="navbarMiddle">
+          middle
+      </div>
+
+      <div class="navbarRight">
+          right
+      </div>
+
+
+  </div>
+    <div class="bottomNavbarWrapper">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+@import '../styles/style.scss';
+
+.wholeNavbarWrapper {
+  position: sticky;
+  position: -webkit-sticky; /* for Safari */
+  align-self: flex-start; /* <-- this is the fix */
+  top: 0;
+}
+
+.topNavbarWrapper {
+  display: flex;
+  flex-direction: row;
+  background-color: #8D1F40;
+  justify-content: center;
+   align-items: center;
+  height: 40px;
+  // position: sticky;
+  // position: -webkit-sticky; /* for Safari */
+  // align-self: flex-start; /* <-- this is the fix */
+  // top: 0;
+}
+
+.navbarLeft {
+  color: white;
+  font-weight: bold;
+  flex: .333;
+}
+
+.navbarMiddle {
+  flex: .333;
+  color: white;
+  font-weight: bold;
+}
+
+.navbarRight {
+  color: white;
+  font-weight: bold;
+  flex: .333;
+}
+
+.bottomNavbarWrapper {
+  background-color: lightgreen;
+
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <Navbar/>
+    <img class='splashLogo' alt="Vue logo" src="../assets/newLogo.png">
+
+    <div class='one'>ONE</div>
+
+ 
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
+
+<style lang='scss'>
+@import '../styles/style.scss';
+
+.one {
+  background-color: $primary;
+  color: $primary;
+  @include flexCenter();
+}
+
+.two {
+  background-color: $secondary;
+  @include flexCenter();
+}
+
+.three {
+  background-color: $thirdly;
+  @include flexCenter();
+}
+
+.splashLogo {
+  width: 260px;
+}
+
+</style>
